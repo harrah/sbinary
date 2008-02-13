a7e24af64114030073c0cad37f06b7d138b6fe5a
david.maciver <david.maciver@2051e08b-d245-0410-b9dc-81ba679ef65e>
2008-02-13 00:46:51 +0000

    Saving equality.scala for posterity prior to deleting.

--- templates/equality.scala
+++ templates/equality.scala
@@ -1,4 +1,5 @@
 package sbinary.equality;
+import scala.collection._;
 
 trait Eq[-T]{
   def equal(x : T, y : T) : Boolean;
@@ -29,5 +30,27 @@ object Instances{
   implicit def arraysAreEq[T](implicit e : Eq[T]) = new Eq[Array[T]]{
     def equal(x : Array[T], y : Array[T]) = (x.length == y.length) && 
       x.zip(y).forall(u => e.equal(u._1, u._2));
+  }
+
+  implicit def listsAreEq[T](implicit e : Eq[T]) = new Eq[List[T]]{
+    def equal(x : List[T], y : List[T]) = (x, y) match {
+      case (Nil, Nil) => true;
+      case (::(x, xs), ::(y, ys)) => if (!e.equal(x, y)) false 
+                                     else equal(xs, ys);
+      case _ => false;
+    }
+  }
+
+  /**
+   * Note that value equality of immutable maps is defined purely in terms
+   * of value equality of the *Values*. Keys use their normal equality test.
+   */
+  implicit def immutableMapsAreEq[K, V](implicit e : Eq[V]) = new Eq[immutable.Map[K, V]]{
+    def equal(x : immutable.Map[K, V], y : immutable.Map[K, V]) =
+      if (x.size != y.size) false;
+      else x.forall((u : (K, V)) => y.get(u._1) match {
+        case None => false;
+        case Some(v) => e.equal(u._1, v);
+      })    
   }
 }
