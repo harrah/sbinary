2788c1c83a69f2e7abf85ce6456f2ac0c030d579
Mark Harrah <dmharrah@gmail.com>
2010-01-24 14:27:39 -0500

    use sbt for build

---
+++ project/build/SBinaryProject.scala
@@ -0,0 +1,27 @@
+import sbt._
+
+class SBinaryProject(info: ProjectInfo) extends DefaultProject(info)
+{
+	val fmppConf = config("fmpp")
+	val fmppDep = "net.sourceforge.fmpp" % "fmpp" % "0.9.13" % "fmpp"
+	val sc = "org.scala-tools.testing" % "scalacheck" % "1.5" % "test"
+
+	def srcManaged = path("src_managed")
+	override def mainScalaSourcePath = srcManaged / "main"
+	override def testScalaSourcePath = srcManaged / "test"
+
+	def fmppArgs(output: Path) = "--ignore-temporary-files" :: "-O" :: output.absolutePath :: Nil
+	def templates = sources("src")
+	def testTemplates = sources("test-src")
+	def fmppClasspath = configurationClasspath(fmppConf)
+
+	override def compileAction = super.compileAction dependsOn(template)
+	override def testCompileAction = super.testCompileAction dependsOn(testTemplate)
+
+	lazy val setupManaged = task { FileUtilities.clean(srcManaged, log); FileUtilities.createDirectory(srcManaged, log) }
+	lazy val template = fmppTask(fmppArgs(mainScalaSourcePath), templates) dependsOn(setupManaged)
+	lazy val testTemplate = fmppTask(fmppArgs(testScalaSourcePath), templates) dependsOn(setupManaged)
+
+	def fmppTask(args: List[String], sources: PathFinder) =
+		runTask(Some("fmpp.tools.CommandLine"), fmppClasspath, args ::: sources.get.map(_.absolutePath).toList)
+}
