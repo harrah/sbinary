f53ad5bf9adb20fa8abab33c3407babb2a2ab76f
Mark Harrah <dmharrah@gmail.com>
2010-01-25 22:54:33 -0500

    Publishing + testing

--- project/build/SBinaryProject.scala
+++ project/build/SBinaryProject.scala
@@ -5,12 +5,14 @@ class SBinaryProject(info: ProjectInfo) extends DefaultProject(info)
 	val fmppConf = config("fmpp")
 	val fmppDep = "net.sourceforge.fmpp" % "fmpp" % "0.9.13" % "fmpp"
 	val sc = "org.scala-tools.testing" % "scalacheck" % "1.5" % "test"
+
+	// to be removed in sbt 0.6.12, which should automatically add this
 	val compat = "org.scala-tools.sbt" %% "test-compat" % "0.4" % "test"
+	override def testFrameworks = new TestFramework("sbt.impl.ScalaCheckFramework") :: Nil
 
 	def srcManaged = path("src_managed")
 	override def mainScalaSourcePath = srcManaged / "main"
 	override def testScalaSourcePath = srcManaged / "test"
-	override def testFrameworks = new TestFramework("sbt.impl.ScalaCheckFramework") :: Nil
 
 	def fmppArgs(output: Path) = "--ignore-temporary-files" :: "-O" :: output.absolutePath :: Nil
 	def templates = sources("src")
@@ -26,4 +28,9 @@ class SBinaryProject(info: ProjectInfo) extends DefaultProject(info)
 
 	def fmppTask(args: List[String], sources: PathFinder) =
 		runTask(Some("fmpp.tools.CommandLine"), fmppClasspath, args ::: sources.get.map(_.absolutePath).toList)
+
+	// publishing
+	override def managedStyle = ManagedStyle.Maven
+	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
