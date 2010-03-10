5b6eb350034b251f957b9e0a6ae74bf68beb897d
Mark Harrah <dmharrah@gmail.com>
2010-03-09 23:04:03 -0500

    pull ScalaCheck version based on Scala version used to build

--- project/build/SBinaryProject.scala
+++ project/build/SBinaryProject.scala
@@ -2,7 +2,9 @@ import sbt._
 
 class SBinaryProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject
 {
-	//val sc = "org.scala-tools.testing" %% "scalacheck" % "1.7-SNAPSHOT" % "test"
+	val sc =
+		if(buildScalaVersion.startsWith("2.7")) "org.scala-tools.testing" %% "scalacheck" % "1.6" % "test"
+		else "org.scala-tools.testing" % "scalacheck" % "1.7-SNAPSHOT" % "test" from("http://scalacheck.googlecode.com/files/scalacheck_2.8.0.Beta1-1.7-SNAPSHOT.jar")
 
 	override def mainResources = super.mainResources +++ "LICENSE"
 
