bb11109f2a85d68f5b508b6bf71f9c50c4bdd517
Mark Harrah <dmharrah@gmail.com>
2010-04-20 10:54:47 -0400

    New example project in examples/bt/.  Moved main project to 'core'.

---
+++ examples/bt/BT.scala
@@ -0,0 +1,61 @@
+package sbintest
+
+import sbinary.DefaultProtocol._
+import sbinary.Operations._
+import sbinary._
+
+	// Binary tree data types that will be
+	//   created and converted to/from byte arrays
+
+sealed abstract class BT;
+case class Bin(left : BT, right : BT) extends BT;
+case class Leaf(label : String) extends BT;
+
+	// Custom protocol for (de)serializing binary trees
+
+object MyProtocol extends DefaultProtocol {
+	implicit object BTFormat extends Format[BT]{
+		def reads(in : Input) = read[Byte](in) match {
+			case 0 => Bin(reads(in), reads(in));
+			case _ => Leaf(read[String](in))
+		}
+
+		def writes(out : Output, value : BT) = value match {
+			case Bin(left, right) => write[Byte](out, 0); writes(out, left); writes(out, right);
+			case Leaf(label) => write[Byte](out, 1); write(out, label);
+		}
+	}
+}
+
+	// test runner
+
+object main {
+	def main(args : Array[String]) : Unit = {
+		val input = List("foo", "bar", "baz")
+		println(input)
+
+		val binary = toByteArray(input)
+		println(showArray(binary))
+
+		val normalAgain = fromByteArray[List[String]](binary)
+		println(normalAgain)
+
+		toFile(input)(new java.io.File("foo.bin"))
+		println(fromFile[List[String]](new java.io.File("foo.bin")))
+
+		import MyProtocol._
+
+		val someTree = Bin(Leaf("foo"), Bin(Leaf("bar"), Leaf("baz")))
+		println(someTree)
+
+		val someTreeInAByteArray = toByteArray[BT](someTree)
+		println(showArray(someTreeInAByteArray))
+
+		val theSameTree = fromByteArray[BT](someTreeInAByteArray)
+		println(theSameTree)
+	}
+	// utility methods for printing a byte array
+	def showArray(b: Array[Byte]) = b.map(showByte).mkString(" ")
+	def showByte(b: Byte) = pad( ((b+256) % 256).toHexString )
+	def pad(s: String) = if(s.length == 1) "0" + s else s
+}
--- project/build/SBinaryProject.scala
+++ project/build/SBinaryProject.scala
@@ -1,15 +1,30 @@
 import sbt._
 
-class SBinaryProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject
+class SBinaryProject(info: ProjectInfo) extends ParentProject(info) with NoPublish
 {
-	val sc =
-		if(buildScalaVersion.startsWith("2.7")) "org.scala-tools.testing" %% "scalacheck" % "1.6" % "test"
-		else "org.scala-tools.testing" % "scalacheck" % "1.7-SNAPSHOT" % "test" from("http://scalacheck.googlecode.com/files/scalacheck_2.8.0.Beta1-1.7-SNAPSHOT.jar")
-
-	override def mainResources = super.mainResources +++ "LICENSE"
-
 	// publishing
 	override def managedStyle = ManagedStyle.Maven
 	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+	lazy val core = project("core", "SBinary", new CoreProject(_))
+	lazy val treeExample = project("examples" / "bt", "Binary Tree Example", new ExampleProject(_), core)
+
+	class ExampleProject(info: ProjectInfo) extends DefaultProject(info) with NoPublish
+	{
+		override def scratch = true
+	}
+	class CoreProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject
+	{
+		val sc =
+			if(buildScalaVersion.startsWith("2.7")) "org.scala-tools.testing" %% "scalacheck" % "1.6" % "test"
+			else "org.scala-tools.testing" % "scalacheck" % "1.7-SNAPSHOT" % "test" from("http://scalacheck.googlecode.com/files/scalacheck_2.8.0.Beta1-1.7-SNAPSHOT.jar")
+
+		override def mainResources = super.mainResources +++ "LICENSE"
+	}
+}
+trait NoPublish extends BasicManagedProject
+{
+	override def deliverAction = publishAction
+	override def publishAction = task { None }
 }
