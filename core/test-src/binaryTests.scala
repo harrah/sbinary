b475012b402bc3b89650fd3d97a8ccfd8718cf70
Antonio Cunei <antonio.cunei@typesafe.com>
2013-07-17 04:48:48 +0200

    Another change to binaryTests.scala
    Enumerations no longer accept a List[String]

--- core/test-src/binaryTests.scala
+++ core/test-src/binaryTests.scala
@@ -141,7 +141,7 @@ object FormatTests extends Properties("Formats"){
   implicit def arbitraryArray[T](implicit arb : Arbitrary[T], mf: scala.reflect.Manifest[T]) : Arbitrary[Array[T]] =
      Arbitrary(arbitrary[List[T]].map((x : List[T]) => x.toArray[T]));
 
-  implicit val arbitraryEnumeration : Arbitrary[Enumeration] = Arbitrary(arbitrary[List[String]].map(x => new Enumeration(x : _*){}));
+  implicit val arbitraryEnumeration : Arbitrary[Enumeration] = Arbitrary(arbitrary[List[String]].map(x => new Enumeration(x.size){}));
 
   implicit def orderedOption[T](opt : Option[T])(implicit ord : Ordering[T]) : Ordered[Option[T]] = new Ordered[Option[T]]{
     def compare(that : Option[T]) = (opt, that) match {
