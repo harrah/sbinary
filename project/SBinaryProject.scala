1a14b2bbb0df9f31ee4dc07ebd15bd54d71c0f86
Mark Harrah <dmharrah@gmail.com>
2013-07-18 17:41:06 -0400

    0.4.2

--- project/SBinaryProject.scala
+++ project/SBinaryProject.scala
@@ -9,7 +9,7 @@ object SBinaryProject extends Build
 
 	lazy val commonSettings: Seq[Setting[_]] = Seq(
 		organization := "org.scala-tools.sbinary",
-		version := "0.4.2-SNAPSHOT",
+		version := "0.4.2",
 		scalaVersion := "2.10.2"
 	)
 
